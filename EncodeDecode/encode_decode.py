4682c3ca228ffab92b369716890584f9bb16b071
nto4 <mehmet.basaran@gazi.edu.tr>
2022-04-09 20:10:21 +0300

    Update encode_decode.py
    added comments

--- EncodeDecode/encode_decode.py
+++ EncodeDecode/encode_decode.py
@@ -6,42 +6,58 @@ import os
 DELIMITER = "//-_-_-//"
 ENCODING = "utf-8"
 
+# for added  files after EOF in pdf
 def put(args):
+    # get pdf file name
     targetFile = args[0]
+    # get path pdf file
     targetFileName = os.path.basename(targetFile)
+    # create patched (pdf + secret file) file name
     patchedFileName = targetFileName.split(".")[0] + "_patched." + targetFileName.split(".")[1]
-
+    # create patched file
     with open(targetFile, "rb") as hostFile, open(patchedFileName, "wb+") as patchedFile:
+        # read original pdf and wrie in new one
         patchedFile.write(hostFile.read())
+        
         index = 1
+        # join a loop we can hide multiple files
         while index < len(args):
             sourceFile = args[index]
             sourceFileName = os.path.basename(sourceFile)
             print("trying to hide", sourceFile, "...")
+            # for each file for the secret
             with open(sourceFile, "rb") as fileToHide:
+                # write source file name + our DELIMITER chracrters and write encding secret file read as binary write as asci
                 patchedFile.write(binascii.hexlify(bytes(sourceFileName + DELIMITER, ENCODING)))
                 patchedFile.write(binascii.hexlify(fileToHide.read()))
                 patchedFile.write(binascii.hexlify(bytes(sourceFileName + DELIMITER, ENCODING)))
                 print("!!! ", sourceFileName, "hidden! in ", targetFile )
             index += 1
-
+    
     util = TimeUtil()
     util.setModTime( patchedFileName, util.getModTime(targetFile) )
 
+    
+# extract hidden file    
 def get(args):
+    # open pdf hidden contain secret files
     with open(args[0], "rb") as targetFile:
         fileContent = targetFile.read()
+        # detect EOF
         fileContentSplit = fileContent.split(bytes("%%EOF", ENCODING))
+        # detect hidden file lenght
         fileContent = fileContentSplit[len(fileContentSplit)-1]
+        # extracrt hidden content
         fileContent = binascii.unhexlify(fileContent.strip())
 
     if fileContent is None:
         print("something went wrong ...")
         exit
-
+    # split first part is file name second part is file content
     hiddenFiles = fileContent.split(bytes(DELIMITER, ENCODING))
     fileName = None
     fileContent = None
+    # we can extracrt multiple files so we in loop
     for index, element in enumerate(hiddenFiles):
         if index % 2 != 0:
             print("extract", fileName, "...")
